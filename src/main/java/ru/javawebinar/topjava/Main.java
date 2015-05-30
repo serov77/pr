3428c79ffd78b780669e5abb465cc94e05a2a1f3
serov77 <serov44@gmail.com>
2015-05-30 15:41:07 +0300

    1

--- src/main/java/ru/javawebinar/topjava/Main.java
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -2,6 +2,6 @@ package ru.javawebinar.topjava;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.format("Hello T2opjavaq!");
+        System.out.format("1Hello T2opjavaq!");
     }
 }
